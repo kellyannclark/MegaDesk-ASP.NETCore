42e6ba51a7cb52736e64eff8802f903c974ae3ca
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-02-29 20:07:48 -0700

    Added the models

---
+++ Models/DeskQuote.cs
@@ -0,0 +1,18 @@
+﻿namespace MegaDesk_ASP.NET_Core.Models
+{
+    public class DeskQuote
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        private readonly ILogger<DeskQuote> _logger;
+        public Desk Desk { get; set; }
+        public int RushDays { get; set; }
+        public int QuoteAmount { get; set; }
+
+        public DeskQuote(ILogger<DeskQuote> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
