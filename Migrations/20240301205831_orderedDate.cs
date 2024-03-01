b74120f224b9af88575a6aff00af8407c98869bd
oliviaWil <olivia@tribewilson.com>
2024-03-01 15:02:04 -0600

    added some validation and date ordered field

---
+++ Migrations/20240301205831_orderedDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaDesk_ASP.NET_Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class orderedDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "orderedDate",
+                table: "DeskQuote",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "orderedDate",
+                table: "DeskQuote");
+        }
+    }
+}
