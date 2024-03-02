d591d837954fbea532079d81bfcf9bdd0571b218
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-03-02 14:19:11 -0700

    The most significant changes in the code are the addition of search and sort functionalities in the `Index.cshtml` and `Index.cshtml.cs` files. The search form in `Index.cshtml` allows users to filter desk quotes by name, while the table headers for 'Name' and 'Ordered Date' now include sorting functionality. In `Index.cshtml.cs`, two new properties, `SearchString` and `SortOrder`, have been added to implement the search and sort functionalities in the `OnGetAsync` method.
    List of changes:
    
    1. `Index.cshtml`:
       - Added a search form to filter desk quotes by name.
       - Updated the table headers for 'Name' and 'Ordered Date' to include sorting functionality.
    
    2. `Index.cshtml.cs`:
       - Added two new properties, `SearchString` and `SortOrder`, which are bound to the incoming request data.
       - Updated the `OnGetAsync` method to include search functionality that filters the desk quotes based on the `SearchString` property and a switch statement that sorts the desk quotes based on the `SortOrder` property.
       - Added the `Microsoft.AspNetCore.Mvc.Rendering` namespace to the list of using directives.

--- Pages/DeskQuotes/Index.cshtml.cs
+++ Pages/DeskQuotes/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDesk_ASP.NET_Core.Data;
 using MegaDesk_ASP.NET_Core.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDesk_ASP.NET_Core.Pages.DeskQuotes
 {
@@ -21,9 +22,38 @@ namespace MegaDesk_ASP.NET_Core.Pages.DeskQuotes
 
         public IList<DeskQuote> DeskQuote { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SortOrder { get; set; }
+
         public async Task OnGetAsync()
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            var deskQuotes = from d in _context.DeskQuote
+                             select d;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                deskQuotes = deskQuotes.Where(s => EF.Functions.Like(s.Name, $"%{SearchString}%"));
+            }
+
+            switch (SortOrder)
+            {
+                case "name_desc":
+                    deskQuotes = deskQuotes.OrderByDescending(s => s.Name);
+                    break;
+                case "date_asc":
+                    deskQuotes = deskQuotes.OrderBy(s => s.orderedDate);
+                    break;
+                case "date_desc":
+                    deskQuotes = deskQuotes.OrderByDescending(s => s.orderedDate);
+                    break;
+                default:
+                    deskQuotes = deskQuotes.OrderBy(s => s.Name);
+                    break;
+            }
+
+            DeskQuote = await deskQuotes.ToListAsync();
         }
     }
 }
