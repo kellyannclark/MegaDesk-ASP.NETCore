1169f91aef5246c636b7df3110ba532cd0b6e900
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-03-01 21:12:02 -0700

    The most important changes include the modification of the `DeskQuote` class to make certain properties required and the addition of a new method. The `OnPostAsync` method has been updated to include a new line of code. The `_Layout.cshtml` file has been updated to change the title of the webpage and the link in the navbar. Two new files related to a new database migration named "Rating" have been added, but no changes to the database schema are being made in this migration.
    1. The `DeskQuote` class in the `MegaDesk_ASP.NET_Core.Models` namespace has been updated. The `DataType` attribute for `orderedDate`, `Name`, `Width`, `Depth`, `NumberOfDrawers`, `SurfaceMaterial`, and `RushDays` properties have been updated to include `Required`. A new method `GetQuote` has been added to the `DeskQuote` class.
    
    2. The `OnPostAsync` method in the `Create.cshtml.cs` file in the `MegaDesk_ASP.NET_Core.Pages.DeskQuotes` namespace has been updated. A new line of code has been added that sets the `QuoteAmount` property of `DeskQuote` to the result of the `GetQuote` method.
    
    3. The `_Layout.cshtml` file has been updated. The title of the webpage has been changed to "Mega Desk". The link in the navbar now leads to the index page of "Mega Desk Quotes". The copyright text in the footer has been updated.
    
    4. Two new files `20240302025934_Rating.cs` and `20240302025934_Rating.Designer.cs` have been added in the `MegaDesk_ASP.NET_Core.Migrations` namespace. These files are related to a new database migration named "Rating". The `Up` and `Down` methods in the migration file are empty, indicating no changes to the database schema. The designer file describes the current state of the database schema.

--- Models/DeskQuote.cs
+++ Models/DeskQuote.cs
@@ -7,28 +7,112 @@ namespace MegaDesk_ASP.NET_Core.Models
         public int ID { get; set; }
 
         [Display(Name = "Ordered Date")]
-        [DataType(DataType.Date)]
+        [DataType(DataType.Date), Required]
         public DateTime orderedDate { get; set; }
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
-        [Range(24, 96)]
+        [Range(24, 96), Required]
         public int Width { get; set; }
-        [Range(12, 48)]
+        [Range(12, 48), Required]
         public int Depth { get; set; }
-        
+
         [Range(0, 7)]
-        [Display(Name = "Number of Drawers")]
+        [Display(Name = "Number of Drawers"), Required]
         public int NumberOfDrawers { get; set; }
 
-        [Display(Name = "Surface Material")]
+        [Display(Name = "Surface Material"), Required]
         public required string SurfaceMaterial { get; set; }
 
-        [Display(Name = "Rush Days")]
+        [Display(Name = "Rush Days"), Required]
         public int RushDays { get; set; }
 
         [Display(Name = "Quoted Price")]
         public int QuoteAmount { get; set; }
 
         public List<String> AllowedMaterials { get; } = new List<string> { "Laminate", "Oak", "Rosewood", "Veneer", "Pine" };
+
+        public int GetQuote()
+        {
+            int basePrice = 200;
+            int surfaceArea = Width * Depth;
+            int surfacePrice = 0;
+            int drawerPrice = 50 * NumberOfDrawers;
+            int rushPrice = 0;
+
+            if (surfaceArea > 1000)
+            {
+                surfacePrice = surfaceArea - 1000;
+            }
+
+            switch (SurfaceMaterial)
+            {
+                case "Laminate":
+                    surfacePrice += 100;
+                    break;
+                case "Oak":
+                    surfacePrice += 200;
+                    break;
+                case "Rosewood":
+                    surfacePrice += 300;
+                    break;
+                case "Veneer":
+                    surfacePrice += 125;
+                    break;
+                case "Pine":
+                    surfacePrice += 50;
+                    break;
+            }
+
+            if (RushDays != 14)
+            {
+                if (surfaceArea < 1000)
+                {
+                    if (RushDays == 3)
+                    {
+                        rushPrice = 60;
+                    }
+                    else if (RushDays == 5)
+                    {
+                        rushPrice = 40;
+                    }
+                    else if (RushDays == 7)
+                    {
+                        rushPrice = 30;
+                    }
+                }
+                else if (surfaceArea >= 1000 && surfaceArea <= 2000)
+                {
+                    if (RushDays == 3)
+                    {
+                        rushPrice = 70;
+                    }
+                    else if (RushDays == 5)
+                    {
+                        rushPrice = 50;
+                    }
+                    else if (RushDays == 7)
+                    {
+                        rushPrice = 35;
+                    }
+                }
+                else
+                {
+                    if (RushDays == 3)
+                    {
+                        rushPrice = 80;
+                    }
+                    else if (RushDays == 5)
+                    {
+                        rushPrice = 60;
+                    }
+                    else if (RushDays == 7)
+                    {
+                        rushPrice = 40;
+                    }
+                }
+            }
+
+            return basePrice + surfacePrice + drawerPrice + rushPrice;
+        }
     }
 }
